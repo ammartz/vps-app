77f8780bbc3c82f164661ff299fcbd79090adf34
ammartz <atalzubaidi@gmail.com>
2023-03-13 16:52:09 +0300

    db connection test

---
+++ server/db.js
@@ -0,0 +1,18 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "siri_admin",
+  password: "aA@816953719"
+});
+
+
+export default function getUsers(){
+    con.connect(function(err) {
+        if (err) throw err;
+        con.query("SELECT * FROM user", function (err, result, fields) {
+          if (err) throw err;
+          return result;
+        });
+      });
+}
